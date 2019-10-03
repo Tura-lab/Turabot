064a7e280536ede5d81b15097754193c633d307d
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2019-10-03 16:44:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,10 @@ from telegram.ext import MessageHandler, Filters
 
 TOKEN = '957266795:AAHewWLVfyyAYhHMAOPP9zukcT9AENWwrqc'
 
-updater = Updater(token= TOKEN, use_context=True)
+REQUEST_KWARGS={
+    'proxy_url': 'http://196.189.114.117:80/'}
+
+updater = Updater(token= TOKEN, use_context=True,request_kwargs=REQUEST_KWARGS)
 
 
 dispatcher = updater.dispatcher
